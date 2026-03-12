97bdf950e86c03a16b2af71e7f15328dd88c4941
SoheilKoushan <so.koushan@gmail.com>
2026-03-12 16:52:35 +0800

    Added Intro

--- main.py
+++ main.py
@@ -2,4 +2,18 @@
 Project Title: Mini Distributed Log Analytics System (Splunk)
 Author: SK/APh
 Date: 12/03/2026
+
+Introduction:
+Modern cloud infrastructure produces enormous volumes of server logs. These logs contain valuable signals about
+performance, system health, and potential security threats. Large organizations rely on platforms such as Splunk and
+Elastic Stack to collect, process, and analyze these logs across distributed systems.
+
+Here, we will build a simplfied diustributed log ana;lytics platform using Python. The system will simulate multiple
+servers sending logs to a central analytics node, which will parse the data, store it using efficient data structure,
+perform statistical analysis, detect anomalies, and visulaise system behaviour.
+
+The final system resembles a miniature version of production observability pipelines used in industry.
+
+
 '''
+
